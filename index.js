6648f37ca1ba71e93784ea2a9da4a7703d0b37e6
Aman Gupta <50515961+Amz42@users.noreply.github.com>
2019-11-09 02:46:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+window.onload=function () {
+	var information= document.getElementById("information");
+	var d=new Date();
+	var year=d.getFullYear();
+	var date=d.getDate();
+	var month=d.getMonth();
+	if(date>=20 && month==11){ var age= year-2000;}
+	else { var age= year-2001;}
+	information.innerHTML="Hello!!! I'm Aman from Delhi,India. I'm "+age+" years old & pursuing graduation in B.tech (Information Technology) from ABES Engineering College,Ghaziabad,UP. I'm a passionate coder & learner. I have great interest in programming & development. I'm currently learning Full Stack Development & Data Analytics.";
+}
