e527879f745b897a8130d238fa06d5310fb2b15e
Aman Gupta <50515961+Amz42@users.noreply.github.com>
2019-11-10 01:52:20 +0530

    Added loader

--- index.js
+++ index.js
@@ -7,4 +7,9 @@ window.onload=function () {
 	if(date>=20 && month==11){ var age= year-2000;}
 	else { var age= year-2001;}
 	information.innerHTML="Hello!!! I'm Aman from Delhi,India. I'm "+age+" years old & pursuing graduation in B.tech (Information Technology) from ABES Engineering College,Ghaziabad,UP. I'm a passionate coder & learner. I have great interest in programming & development. I'm currently learning Full Stack Development & Data Analytics.";
+
+	// Removing the loader
+	setTimeout(() => {
+		document.body.removeChild(document.getElementById("loader-container"))
+	},2000);
 }
